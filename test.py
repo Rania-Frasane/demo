76e8bea6bb1a15a82b511d86cd463210db92039e
Rania Farsane <149484869+Rania-Frasane@users.noreply.github.com>
2024-12-05 11:42:52 +0100

    Update test.py

--- test.py
+++ test.py
@@ -1,8 +1,30 @@
-# Affiche "hello world"
-print('hello world')
+# Fonction pour demander le nom de l'utilisateur
+def demander_nom():
+    nom = input('Quel est votre nom ? ')
+    while not nom:  # Vérifie si l'utilisateur n'a pas laissé l'entrée vide
+        print("Vous devez entrer un nom.")
+        nom = input('Quel est votre nom ? ')
+    return nom
 
-# Demande à l'utilisateur son nom et stocke la réponse dans la variable 'nom'
-nom = input('Quel est votre nom ? ')
+# Fonction principale du programme
+def programme_principal():
+    # Afficher un message d'accueil
+    print('hello world')
 
-# Affiche un message personnalisé en fonction du nom
-print(f'Bonjour {nom}, content de vous rencontrer!')
+    # Demander le nom à l'utilisateur
+    nom = demander_nom()
+
+    # Afficher un message personnalisé
+    print(f'Bonjour {nom}, content de vous rencontrer!')
+
+    # Poser une autre question
+    age = input('Quel est votre âge ? ')
+    while not age.isdigit():  # Vérifie si l'âge est un nombre
+        print("L'âge doit être un nombre.")
+        age = input('Quel est votre âge ? ')
+    
+    # Afficher un message avec l'âge
+    print(f'Vous avez {age} ans. Génial !')
+
+# Lancer le programme
+programme_principal()
